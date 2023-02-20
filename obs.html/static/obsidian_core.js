1966f40d73eb697fa39d4609450b31dce9be27ea
VojtechMlynar <voj.mlynar@gmail.com>
2023-02-20 18:06:46 +0100

    Spelling errors

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '327138736632509070012432513335669804498'                       // used to check whether the localStorage data is stale
+var gzip_hash = '308031928166248128078451697775578093333'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
